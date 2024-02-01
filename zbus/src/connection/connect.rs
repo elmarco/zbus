3c2ea3a2bc4a705d4aa742c601d7be27c6c3a51e
Marc-André Lureau <marcandre.lureau@redhat.com>
2024-02-01 12:17:30 +0400

    ♻️  zb: add win32 Autolaunch connection

--- zbus/src/connection/connect.rs
+++ zbus/src/connection/connect.rs
@@ -6,6 +6,7 @@ use zbus_address::{transport::Transport, DBusAddr};
 use super::socket::{self, BoxedSplit};
 
 mod macos;
+mod win32;
 
 type ConnectResult = Result<(BoxedSplit, Option<OwnedGuid>)>;
 
@@ -28,6 +29,10 @@ fn connect(addr: &DBusAddr<'_>) -> Pin<Box<dyn Future<Output = ConnectResult>>> 
             Transport::Vsock(v) => socket::vsock::connect(&v).await?.into(),
             #[cfg(target_os = "macos")]
             Transport::Launchd(l) => macos::connect(&l).await?.into(),
+            #[cfg(target_os = "windows")]
+            Transport::Autolaunch(l) => {
+                return win32::connect(&l).await;
+            }
             _ => {
                 // safety: unwrap() for code transition => addr is valid already
                 let legacy: crate::Address = addr.to_string().parse().unwrap();
